8c488d24e2781694cbec93e520bef70bc6711294
daviacandido <daviaraujocandido@gmail.com>
2025-01-16 21:40:06 -0300

    tecla.onkeydown and onkeyup

--- main.js
+++ main.js
@@ -9,7 +9,16 @@ for (count = 0; count < listaDeTeclas.length; count++) {
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
 
-  listaDeTeclas[count].onclick = function () {
+  tecla.onclick = function () {
     tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code == "Space" || evento.code == "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
   };
 }
